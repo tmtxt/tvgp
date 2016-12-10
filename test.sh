ad5fd1aa2cb66c132a90a675f65a6f4f5df9a942
TruongTX <tmtxt@truong-book.local>
2016-12-10 14:48:37 +0700

    update env file port

--- test.sh
+++ test.sh
@@ -4,11 +4,15 @@ export COMPOSE_PROJECT_NAME=tvgp_test
 export COMPOSE_FILE=docker-compose.test.yml
 
 export POSTGRES_SERVER=localhost
-export POSTGRES_PORT=5432
+export POSTGRES_PORT=40001
 export POSTGRES_DATABASE=tvgp
 export POSTGRES_USER=tvgp
 export POSTGRES_ENSURE_STORAGE=true
 
+export API_SERVER_PORT=40002
 export API_SERVER_CODE_RELOADER=false
 export API_SERVER_STACKTRACE_DEPTH=20
 export API_SERVER_SEED_DATA=true
+
+export REDIS_SERVER=localhost
+export REDIS_PORT=40003
