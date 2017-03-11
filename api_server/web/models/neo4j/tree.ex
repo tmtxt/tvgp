5fd6c77f3b0a149aca35d1503e6e1e7598c200c6
TruongTX <me@truongtx.me>
2017-03-11 22:54:02 +0700

    Move to Tree model

---
+++ api_server/web/models/neo4j/tree.ex
@@ -0,0 +1,48 @@
+defmodule ApiServer.Models.Neo4j.Tree do
+
+  alias Neo4j.Sips, as: Neo4j
+
+  @doc """
+  Query the tree data from the root_node_id in neo4j
+  Return a list like this, ordered by length "path". The least path stays first.
+  Each node in the list represent one person in the tree
+  path: path from root node to this node
+  marrage: list of all person ids get married with this person
+  [
+    {
+      "path": [
+        118,
+        119
+      ],
+      "marriage": [],
+      "depth": 1
+    },
+    {
+      "path": [
+        118,
+        119,
+        120
+      ],
+      "marriage": [121],
+      "depth": 2
+    }
+  ]
+  """
+  def get_neo_tree_from_node_id(root_node_id, log_trace) do
+    query = """
+    MATCH p=(root:Person)-[:Father_child|Mother_child *1..5]->(child:Person)
+    WHERE id(root) = #{root_node_id}
+    WITH nodes(p) AS all_nodes,
+    relationships(p) AS all_relationships,
+    length(p) AS depth,
+    extract(n IN (child)-[:Husband_wife|Wife_husband]->(:Person) | last(nodes(n)).person_id) AS marriage
+    RETURN extract(n IN all_nodes | n.person_id) AS `path`,
+    depth AS `depth`,
+    marriage AS `marriage`,
+    last(extract(r IN all_relationships | r.`order`)) AS `last_order`,
+    extract(n IN all_nodes | n.person_id)[-2] AS `last_parent`
+    ORDER BY `depth`, `last_parent`, `last_order`;
+    """
+    Neo4j.query!(Neo4j.conn, query)
+  end
+end
--- api_server/web/services/tree/tree_service.ex
+++ api_server/web/services/tree/tree_service.ex
@@ -3,7 +3,8 @@ defmodule ApiServer.Services.Tree do
 
   alias ApiServer.Models.Neo4j.Person, as: NeoPerson
   alias ApiServer.Models.Postgres.Person, as: PgPerson
-  alias ApiServer.Models.Neo4j.MarriageRelation, as: MarriageRelation
+  alias ApiServer.Models.Neo4j.Tree
+  alias ApiServer.Models.Neo4j.MarriageRelation
 
 
   def get_tree(from_person_id, log_trace) do
@@ -13,7 +14,7 @@ defmodule ApiServer.Services.Tree do
     # construct the root node (the starting of the tree) and query the tree structure from neo4j
     res = Task.yield_many([
       Task.async(fn -> find_root_with_info(root_node, log_trace) end),
-      Task.async(fn -> get_neo_tree_from_node_id(root_node_id, log_trace) end)
+      Task.async(fn -> Tree.get_neo_tree_from_node_id(root_node_id, log_trace) end)
     ])
     [{_, {:ok, root_tree}}, {_, {:ok, neo_tree}}] = res
 
@@ -133,49 +134,6 @@ defmodule ApiServer.Services.Tree do
       marriages: marriages,
       children: %{}
     }
-  end
-
-
-  # Query the tree data from the root_node_id in neo4j
-  # Return a list like this, ordered by length "path". The least path stays first.
-  # Each node in the list represent one person in the tree
-  # path: path from root node to this node
-  # marrage: list of all person ids get married with this person
-  # [
-  #   {
-  #     "path": [
-  #       118,
-  #       119
-  #     ],
-  #     "marriage": [],
-  #     "depth": 1
-  #   },
-  #   {
-  #     "path": [
-  #       118,
-  #       119,
-  #       120
-  #     ],
-  #     "marriage": [121],
-  #     "depth": 2
-  #   }
-  # ]
-  defp get_neo_tree_from_node_id(root_node_id, log_trace) do
-    query = """
-    MATCH p=(root:Person)-[:Father_child|Mother_child *1..5]->(child:Person)
-    WHERE id(root) = #{root_node_id}
-    WITH nodes(p) AS all_nodes,
-    relationships(p) AS all_relationships,
-    length(p) AS depth,
-    extract(n IN (child)-[:Husband_wife|Wife_husband]->(:Person) | last(nodes(n)).person_id) AS marriage
-    RETURN extract(n IN all_nodes | n.person_id) AS `path`,
-    depth AS `depth`,
-    marriage AS `marriage`,
-    last(extract(r IN all_relationships | r.`order`)) AS `last_order`,
-    extract(n IN all_nodes | n.person_id)[-2] AS `last_parent`
-    ORDER BY `depth`, `last_parent`, `last_order`;
-    """
-    Neo4j.query!(Neo4j.conn, query)
   end
 
 end
