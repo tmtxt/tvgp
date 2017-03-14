addbc540333d79bb12a840a600cee8408da13009
TruongTX <me@truongtx.me>
2017-03-14 23:13:47 +0700

    Assign id for the tree to identify

--- api_server/web/controllers/tree.ex
+++ api_server/web/controllers/tree.ex
@@ -4,22 +4,34 @@ defmodule ApiServer.TreeController do
 
   @doc """
   Get the tree from default root
+  Response the tree with id: "root"
+  For tree structure, refer to TreeService.construct_tree
   """
   def get_tree_from_default_root(conn, _params) do
     log_trace = conn.assigns.log_trace
     tree = TreeService.get_tree(log_trace)
+    |> Map.put(:id, "root")
     json(conn, tree)
   end
 
 
+  @doc """
+  Get the tree from custom person
+  Response the tree with id: ^person_id
+  For tree structure, refer to TreeService.construct_tree
+  """
   def get_tree_from_person(conn, params) do
     log_trace = conn.assigns.log_trace
 
-    %{"person_id" => person_id} = params
-    person_id = String.to_integer(person_id)
+    person_id = params
+    |> Map.get("person_id")
+    |> String.to_integer()
     LogTrace.add(log_trace, :info, "get_tree_from_person", "From person id #{person_id}")
 
-    tree = TreeService.get_tree(person_id, log_trace)
+    tree = person_id
+    |> TreeService.get_tree(log_trace)
+    |> Map.put(:id, person_id)
+
     json(conn, tree)
   end
 end
