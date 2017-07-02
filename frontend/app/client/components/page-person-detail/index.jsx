ca644afdbd150a577c56b9b7b9bead918aa15bd0
TruongTX <me@truongtx.me>
2017-07-02 16:38:05 +0700

    Wrap PersonDetailPage in withPersonInfo hoc

--- frontend/app/client/components/page-person-detail/index.jsx
+++ frontend/app/client/components/page-person-detail/index.jsx
@@ -1,22 +1,27 @@
 // @flow
 import React, { Component } from 'react';
 
-export class PagePersonDetail extends Component {
-  static displayName = 'PagePersonDetail';
+import withPersonInfo from 'client/components/person/with-person-info';
+
+import PersonDetailPage from './person-detail-page';
+
+const PersonDetailPageComponent = withPersonInfo(PersonDetailPage);
+
+export class PersonDetailPageWrapper extends Component {
+  static displayName = 'PersonDetailPageWrapper';
 
   props: {
-
+    params: {
+      personId: string
+    }
   }
 
   render() {
+    let personId = this.props.params.personId;
+    personId = parseInt(personId, 10);
 
-
-    return (
-      <div>
-        hello
-      </div>
-    );
+    return <PersonDetailPageComponent personId={personId} />;
   }
 }
 
-export default PagePersonDetail;
+export default PersonDetailPageWrapper;
---
+++ frontend/app/client/components/page-person-detail/person-detail-page.jsx
@@ -0,0 +1,20 @@
+// @flow
+import React, { Component } from 'react';
+
+export class PersonDetailPage extends Component {
+  static displayName = 'PersonDetailPage';
+
+  props: {
+
+  }
+
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+}
+
+export default PersonDetailPage;
